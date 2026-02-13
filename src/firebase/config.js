912f2e8baa56f886b8d1318f39995245063c5260
Muhammad Ali <139394180+RaeezAli@users.noreply.github.com>
2026-02-13 15:20:01 +0500

    Move Firebase config to environment variables to resolve security leak

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAQqVr1ZbLrmafuzi6vH5vhfM6x5cqr_cs",
-  authDomain: "chat-web-app-b1dfa.firebaseapp.com",
-  projectId: "chat-web-app-b1dfa",
-  storageBucket: "chat-web-app-b1dfa.firebasestorage.app",
-  messagingSenderId: "15341298272",
-  appId: "1:15341298272:web:20082c85a22abdc2341905"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
